83f8408fab893a6387b5e5a4bdc7f096c15d7e85
wolfather <so.israelweb@gmail.com>
2023-02-12 17:57:38 -0300

    refactor create form hook

--- src/components/CForm/index.tsx
+++ src/components/CForm/index.tsx
@@ -1,15 +1,12 @@
-import { FC, FormEvent, useCallback, useContext, useMemo, useState } from "react";
+import { FC, FormEvent, useMemo, useState } from "react";
 
 import { CButton } from "../CButton";
 import { CTextarea } from "../CTextarea";
 import { CSelect } from "../CSelect";
 import { CATEGORIES } from "../../enum/categories";
 import { NOTIFICATIONS } from '../../enum/notifications';
-import { UserContext, UserContextProp } from "../../providers/user.provider";
-import { AppLoggerContext, LoggerContextProp } from "../../providers/applogger.provider";
-import { LogFactory } from "../../factory/log.factory";
-import { NotificationContext, NotificationContextProps } from "../CNotification";
 import { styles } from "./styles";
+import { useSubmit } from "../../hooks/usesubmit";
 
 export const CForm: FC<{}> = () => {
     const [message, setMessage] = useState<string>('');
@@ -18,10 +15,9 @@ export const CForm: FC<{}> = () => {
 
     const MAX_TEXTAREA_VALUES = Object.freeze(100);
     
-    const { userSelected, setUserSelected } = useContext<UserContextProp>(UserContext);
-    const { setLogs } = useContext<LoggerContextProp>(AppLoggerContext);
-    const { setNotify, setNotificationData } = useContext<NotificationContextProps>(NotificationContext)
-    
+    const { userSelected, onSubmitForm } = useSubmit({
+        notificationValue, categoryValue, message
+    });
     const validateForm = useMemo(() => {
         return (
             categoryValue !== '' && 
@@ -34,77 +30,51 @@ export const CForm: FC<{}> = () => {
         setMessage('');
         setCategoryValue('');
         setNotificationValue('');
-        setUserSelected({});
     };
 
-    const submitForm = useCallback((e: FormEvent<HTMLFormElement>) => {
+    const submitForm = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
-        const _channels = new Set(userSelected.channels)
-        const _subscribed = new Set(userSelected.subscribed)
-
         if(validateForm && userSelected.id) {
-            _channels.add(notificationValue)
-            _subscribed.add(categoryValue)
-
-            setUserSelected(prev => ({
-                ...prev, 
-                channels : Array.from(_channels),
-                subscribed: Array.from(_subscribed)
-            }));
-
-            const userLogData = LogFactory(
-                userSelected, 
-                notificationValue, 
-                categoryValue,
-                message
-            );
-
-            setLogs(prev => [...prev, userLogData]);
-            
-            setNotificationData({
-                channel: notificationValue,
-                subscription: categoryValue,
-                user: userSelected
-            });
+            onSubmitForm();
             
             resetValues();
-            
-            setNotify(true);
         }
-    }, [userSelected, notificationValue, categoryValue, message]);
+    };
 
-    return (<section className={styles.container}>
-        <form onSubmit={(e) => submitForm(e)}>
-            <div className={styles.fields}>
-                <CSelect 
-                    selectType='category' 
-                    optionsData={CATEGORIES}
-                    setSelectValue={setCategoryValue}
-                    selectValue={categoryValue} />
-            </div>
-            <div className={styles.fields}>
-                <CSelect 
-                    selectType='notification' 
-                    optionsData={NOTIFICATIONS}
-                    setSelectValue={setNotificationValue}
-                    selectValue={notificationValue} />
-            </div>
-            <div className={styles.fields}>
-                <CTextarea 
-                    charLimit={MAX_TEXTAREA_VALUES} 
-                    setTextareaMessage={setMessage} 
-                    textareaMessage={message}
-                    rows={5}
-                    cols={50} />
-            </div>
+    return (
+        <section className={styles.container}>
+            <form onSubmit={(e) => submitForm(e)}>
+                <div className={styles.fields}>
+                    <CSelect 
+                        selectType='category' 
+                        optionsData={CATEGORIES}
+                        setSelectValue={setCategoryValue}
+                        selectValue={categoryValue} />
+                </div>
+                <div className={styles.fields}>
+                    <CSelect 
+                        selectType='notification' 
+                        optionsData={NOTIFICATIONS}
+                        setSelectValue={setNotificationValue}
+                        selectValue={notificationValue} />
+                </div>
+                <div className={styles.fields}>
+                    <CTextarea 
+                        charLimit={MAX_TEXTAREA_VALUES} 
+                        setTextareaMessage={setMessage} 
+                        textareaMessage={message}
+                        rows={5}
+                        cols={50} />
+                </div>
 
-            <div className={styles.buttonContainer}>
-                <CButton 
-                    label='Submit' 
-                    buttonType='submit'
-                    isDisabled={!validateForm} />
-            </div>
-        </form>
-    </section>)
+                <div className={styles.buttonContainer}>
+                    <CButton 
+                        label='Submit' 
+                        buttonType='submit'
+                        isDisabled={!validateForm} />
+                </div>
+            </form>
+        </section>
+    );
 }
