3be769e588eab5656b60009490f5dc37b4c7734f
Israel <so.israelweb@gmail.com>
2024-07-01 09:42:38 -0300

    Update table.tsx

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -1,284 +1,170 @@
-'use client'
+export interface TransactionsEntity {
+    dateSettlement: string
+    datePreference: string
+    code: {
+        code: string
+    },
+    name: string
+    amount: number
+}
 
-import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
+export interface ResponseFactory {
+    accountId: string
+    transactions: TransactionsEntity[]
+}
 
-import {
-  ColumnDef,
-  flexRender,
-  getCoreRowModel,
-  getSortedRowModel,
-  OnChangeFn,
-  Row,
-  SortingState,
-  useReactTable,
-} from '@tanstack/react-table'
 
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table"
-
-import { useVirtualizer } from '@tanstack/react-virtual'
-
-import { fetchData, Person } from  './fakeapi/getdata'
-
-export default function Home() {
-  const tableContainerRef = useRef<HTMLDivElement>(null)
-
-  const [sorting, setSorting] = useState<SortingState>([])
-  const [page, setPage] = useState(1)
-  const [data, setData] = useState<Person[]>([])
-  const [totalRowCount, setTotalRowCount] = useState<number>(0)
-  const [hasNextPage, setHasNextPage] = useState(false)
-  const [isLoading, setIsLoading] = useState(false)
-
-  const columns = useMemo<ColumnDef<Person>[]>(
-    () => [
-      {
-        accessorKey: 'id',
-        header: 'ID',
-        size: 60,
-      },
-      {
-        accessorKey: 'firstName',
-        cell: info => info.getValue(),
-      },
-      {
-        accessorFn: row => row.lastName,
-        id: 'lastName',
-        cell: info => info.getValue(),
-        header: () => <span>Last Name</span>,
-      },
-      {
-        accessorKey: 'age',
-        header: () => 'Age',
-        size: 50,
-      },
-      {
-        accessorKey: 'visits',
-        header: () => <span>Visits</span>,
-        size: 50,
-      },
-      {
-        accessorKey: 'status',
-        header: 'Status',
-      },
-      {
-        accessorKey: 'progress',
-        header: 'Profile Progress',
-        size: 80,
-      },
-      {
-        accessorKey: 'createdAt',
-        header: 'Created At',
-        cell: info => info.getValue<Date>().toLocaleString(),
-        size: 200,
-      },
-    ],
-    []
-  )
-
-  const fetchNextPage = useCallback(() => {
-    setIsLoading(true)
-    fetchData(page, sorting)
-    .then(res => {
-      setData(prev => [...prev, res.data].flat())
-      setTotalRowCount(res.meta.totalRowCount)
-      setHasNextPage(res.hasNext)
-    })
-    .catch(console.log)
-    .finally(() => {
-      setIsLoading(false)
-    })
-  }, [page, sorting])
-
-  useEffect(() => {
-    fetchData(1, sorting)
-      .then(res => {
-        setData(res.data)
-        setTotalRowCount(res.meta.totalRowCount)
-        setHasNextPage(res.hasNext)
-      })
-      .catch(console.log)
-
-  }, [sorting])
-  // console.log({data})
-
-  const totalFetched = data.length
-  console.log({ totalFetched })
-
-  const fetchMoreOnBottomReached = useCallback(
-    (tableContainerRef?: HTMLDivElement | null) => {
-      if (tableContainerRef) {
-        const { scrollHeight, scrollTop, clientHeight } = tableContainerRef
-        console.log({ totalFetched, totalRowCount }, (scrollHeight - scrollTop - clientHeight), 500)
-        if (
-          (scrollHeight - scrollTop - clientHeight) < 500 &&
-          !isLoading &&
-          totalFetched < totalRowCount
-        ) {
-          setPage(prev => (prev+1))
-          //fetchNextPage()
+const makeItems = () => {
+    const totalItems = 36
+    const response = {
+        accountId: 'account123',
+        transactions: [],
+        pageInfo: {
+            isFirst: true,
+            hasNext: true
         }
-      }
-    },
-    [isLoading, totalFetched, totalRowCount]
-  )
+    };
 
-  useEffect(() => {
-    fetchMoreOnBottomReached(tableContainerRef.current)
-  }, [fetchMoreOnBottomReached])
-
-  const table = useReactTable({
-    data,
-    columns,
-    state: {
-      sorting,
-    },
-    getCoreRowModel: getCoreRowModel(),
-    getSortedRowModel: getSortedRowModel(),
-    manualSorting: true,
-    debugTable: true,
-  })
-
-  const handleSortingChange: OnChangeFn<SortingState> = updater => {
-    setSorting(updater)
-    if (!!table.getRowModel().rows.length) {
-      rowVirtualizer.scrollToIndex?.(0)
+    const transactions = []
+    for(let i = 0; i < totalItems; i++) {
+        transactions.push({
+            dateSettlement: '21-01-2023',
+            datePreference: '22-01-2023',
+            code: {
+                code: `CODE-${i}`
+            },
+            name: 'PRODUCT-INVESTMENT',
+            amount: Math.round(Math.random() * 2000).toFixed(2)
+        })
     }
+
+    return {...response, transactions}
+}
+
+export function getTransactionsFactory(page: number) {
+    const chunkSize = 10;
+    const { transactions, pageInfo, accountId } = makeItems()
+    let pageChunkIndex = 0;
+    const result = transactions.reduce((acc, item, index) => {
+        const chunkIndex = Math.floor(index / chunkSize);
+        pageChunkIndex = chunkIndex
+        if (!acc[chunkIndex]) {
+            acc[chunkIndex] = [];
+        }
+        
+        acc[chunkIndex].push(item);
+        
+        return acc;
+    }, []);
+    
+    if(page < pageChunkIndex) {
+        const _pageInfo = { ...pageInfo, hasNext: page < pageChunkIndex }
+        return { accountId, pageInfo: _pageInfo, transactions: result }
+    }
+    return {}
+}
+
+
+import { ColumnSort, SortingState } from '@tanstack/react-table'
+
+export type Person = {
+  id: number
+  firstName: string
+  lastName: string
+  age: number
+  visits: number
+  progress: number
+  status: string
+  createdAt: string
+}
+
+export type PersonApiResponse = {
+  data: Person[]
+  meta: {
+    totalRowCount: number
+  }
+}
+
+const range = (len: number) => {
+  const arr: number[] = []
+  for (let i = 0; i < len; i++) {
+    arr.push(i)
+  }
+  return arr
+}
+
+const newPerson = (index: number): Person => {
+  return {
+    id: index + 1,
+    firstName: `abc ${index +1}`,
+    lastName: `cxz  ${index +1}`,
+    age: index + 1,
+    visits:  index +1,
+    progress: 80,
+    createdAt: '22-10-2022',
+    status: 'up',
+  }
+}
+
+export function makeData(lens: number) {
+    const items = []
+    for(let i = 0; i < lens; i++) {
+        items.push(newPerson(i))
+    }
+    console.log(items[12])
+
+    return items
+}
+
+const data = makeData(1000)
+
+//simulates a backend api
+export const fetchData = async (
+  start: number,
+  sorting: SortingState
+) => {
+    console.log({start})
+    const size = 10
+    const startIndex = (start * size) - size
+    let isLoading = false
+
+  const dbData = [...data]
+  if (sorting.length) {
+    const sort = sorting[0] as ColumnSort
+    const { id, desc } = sort as { id: keyof Person; desc: boolean }
+    dbData.sort((a, b) => {
+      if (desc) {
+        return a[id] < b[id] ? 1 : -1
+      }
+      return a[id] > b[id] ? 1 : -1
+    })
   }
 
-  table.setOptions(prev => ({
-    ...prev,
-    onSortingChange: handleSortingChange,
-  }))
+  //simulate a backend api
+  isLoading = true
+  await new Promise(resolve => setTimeout(resolve, 430))
+  isLoading = false
 
-  const { rows } = table.getRowModel()
-
-  const rowVirtualizer = useVirtualizer({
-    count: rows.length,
-    estimateSize: () => 33, //estimate row height for accurate scrollbar dragging
-    getScrollElement: () => tableContainerRef.current,
-    measureElement:
-      typeof window !== 'undefined' &&
-      navigator.userAgent.indexOf('Firefox') === -1
-        ? element => element?.getBoundingClientRect().height
-        : undefined,
-    overscan: 5,
-  })
-
-  // if (isLoading) {
-  //   return <>Loading...</>
-  // }
-
-  return (
-    <div className="app">
-      ({data.length} of {totalRowCount} rows fetched)
-      <p>{page}</p>
-      <div
-        className="container"
-        onScroll={e => fetchMoreOnBottomReached(e.target as HTMLDivElement)}
-        ref={tableContainerRef}
-        style={{
-          overflow: 'auto', //our scrollable table container
-          position: 'relative', //needed for sticky header
-          height: '500px', //should be a fixed height
-        }}
-      >
-        <Table style={{ display: 'grid' }}>
-          <TableHeader
-            style={{
-              display: 'grid',
-              position: 'sticky',
-              top: 0,
-              zIndex: 1,
-            }}
-          >
-            {table.getHeaderGroups().map(headerGroup => (
-              <TableRow
-                key={headerGroup.id}
-                style={{ display: 'flex', width: '100%' }}
-              >
-                {headerGroup.headers.map(header => {
-                  return (
-                    <TableHead
-                      key={header.id}
-                      style={{
-                        display: 'flex',
-                        width: header.getSize(),
-                      }}
-                    >
-                      <div
-                        {...{
-                          className: header.column.getCanSort()
-                            ? 'cursor-pointer select-none'
-                            : '',
-                          onClick: header.column.getToggleSortingHandler(),
-                        }}
-                      >
-                        {flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                        {{
-                          asc: '^',
-                          desc: 'v',
-                        }[header.column.getIsSorted() as string] ?? null}
-                      </div>
-                    </TableHead>
-                  )
-                })}
-              </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody
-            style={{
-              display: 'grid',
-              height: `${rowVirtualizer.getTotalSize()}px`,
-              position: 'relative',
-            }}
-          >
-            {rowVirtualizer.getVirtualItems().map(virtualRow => {
-              const row = rows[virtualRow.index] as Row<Person>
-              return (
-                <TableRow
-                  data-index={virtualRow.index}
-                  ref={node => rowVirtualizer.measureElement(node)}
-                  key={row.id}
-                  style={{
-                    display: 'flex',
-                    position: 'absolute',
-                    transform: `translateY(${virtualRow.start}px)`,
-                    width: '100%',
-                  }}
-                >
-                  {row.getVisibleCells().map(cell => {
-                    return (
-                      <TableCell
-                        key={cell.id}
-                        style={{
-                          display: 'flex',
-                          width: cell.column.getSize(),
-                        }}
-                      >
-                        {flexRender(
-                          cell.column.columnDef.cell,
-                          cell.getContext()
-                        )}
-                      </TableCell>
-                    )
-                  })}
-                </TableRow>
-              )
-            })}
-          </TableBody>
-        </Table>
-      </div>
-      {isLoading ? <>{JSON.stringify(hasNextPage)} - {JSON.stringify(isLoading)}</> : <></>}
-    </div>
-  )
+  if(start < Math.floor(data.length / size)) {
+    return {
+        isFirst: start === 0,
+        isLoading,
+        hasNext: start < Math.floor(data.length / size),
+        isFetching: true,
+        data: dbData.slice(startIndex, (start + size) -1),
+        meta: {
+            totalRowCount: dbData.length,
+        },
+    }
+  }
+  return {
+    isFirst: false,
+    hasNext: false,
+    isLoading,
+    isFetching: false,
+    data: [],
+    meta: {
+        totalRowCount: dbData.length,
+    }
+  }
 }
